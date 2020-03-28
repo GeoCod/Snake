daffcbbfa77ead684d619a8fa9ca56277d704a30
Artem <artem.g.vasilchenko@gmail.com>
2020-03-28 21:32:23 +0300

    Сощлан класс Snake
    Добавлена простая реализация змейки

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -24,11 +24,44 @@ namespace Snake
             sym = _sym;
         }
 
+        //для класса Snake
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if(direction == Direction.RIGHT)
+            {
+                x += offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x -= offset;
+            }
+            else if (direction == Direction.DOWN)
+            {
+                y -= offset;
+            }
+            else if ( direction == Direction.UP)
+            {
+                y += offset;
+            }
+        }
 
         public void Draw()
         {
             Console.SetCursorPosition( x, y );
             Console.WriteLine( sym );
         }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
+        }
+
     }
 }
---
+++ Snake/Snake.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake : Figure
+    {
+        public Snake(Point tail, int lenght, Direction direction)
+        {
+            plist = new List<Point>();
+            for(int i = 0; i < lenght; i++)
+            {
+                Point p = new Point(tail);  //добавить конструктор
+                p.Move(i, direction);       //добавить метод
+                plist.Add(p);
+            }
+        }
+    }
+}
