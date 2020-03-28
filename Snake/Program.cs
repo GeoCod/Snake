2cb614ba8d615272b359cd5c5eb9e0bb58496e32
Artem <artem.g.vasilchenko@gmail.com>
2020-03-28 19:07:36 +0300

    Добавлен класс VerticalLine
    Рисует вертикальные линии из символов

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,14 +10,17 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
+/*            Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
-            p2.Draw( );
+            p2.Draw( );*/
 
             HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
             line.Draw();
+
+            VerticalLine line1 = new VerticalLine(15, 25, 7, '#');
+            line1.Draw();
 
             Console.ReadLine();
         }
---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> plist;
+
+        public VerticalLine(int yTop, int yButtom, int x, char sym)
+        {
+            plist = new List<Point>();
+            for(int y = yTop; y <= yButtom; y++)
+            {
+                Point p = new Point(x, y, sym);
+                plist.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in plist)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
